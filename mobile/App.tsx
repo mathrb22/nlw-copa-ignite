a3a03ccfc0ee37d9ae1d86a894e8662721dde2f4
Matheus Ribeiro <mathribe2020@gmail.com>
2022-11-02 02:03:57 -0300

    feat: [mobile] - adding native-base and customising the app theme

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { THEME } from './src/styles/theme';
+import {
+	useFonts,
+	Roboto_400Regular,
+	Roboto_500Medium,
+	Roboto_700Bold,
+} from '@expo-google-fonts/roboto';
+import { Loading } from './src/components/Loading';
+import { SignIn } from './src/screens/Signin';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	const [fontsLoaded] = useFonts({
+		Roboto_400Regular,
+		Roboto_500Medium,
+		Roboto_700Bold,
+	});
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	return (
+		<NativeBaseProvider theme={THEME}>
+			<StatusBar
+				barStyle='light-content'
+				backgroundColor='transparent'
+				translucent
+			/>
+			{fontsLoaded ? <SignIn /> : <Loading />}
+		</NativeBaseProvider>
+	);
+}
